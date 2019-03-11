f2154b584d9fe90e4a2f40063dbeff8b06ff8eee
Rudy Khanwale <chaos.rudy@gmail.com>
2019-03-11 09:33:45 -0700

    Completed Tile Class

--- src/Tile.java
+++ src/Tile.java
@@ -2,10 +2,25 @@
  * This is the class with tiles, for each individual game
  */
 public class Tile {
-// WordHunt
+
 private char letter;
 private int row;
 private int column; 
+
+
+public Tile(char CChar, int xpos, int ypos) {
+	letter = CChar;
+	row = xpos;
+	column = ypos;
+
+}
+
+public Tile(char CChar, int ypos) {
+	letter = CChar; 
+	column = ypos; 
+}
+
+
 
 	
 	
